c2c555cf86123d84d62365790a76547188df2aeb
RobertKraaijeveld <0890289@hr.nl>
2016-05-31 22:44:18 +0200

    Got rid of BaseMapPlot and moved it to main. Cant figure out how to change color and opacity.

--- Opdracht3/src/main/src/main/java/Main/Main.java
+++ Opdracht3/src/main/src/main/java/Main/Main.java
@@ -4,20 +4,64 @@ import Datastructures.GenericPair;
 import Datastructures.Vector3D;
 import FileParsing.FileParser;
 import FileParsing.ParsedFile;
-import Plotting.BasicMapPlot;
 import Plotting.ValueConverter;
 import Utilities.TimeMeasurer;
+import processing.core.PApplet;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 /**
  *
  * @author gover_000
  */
 
-public class Main
+public class Main extends PApplet
 {
+    private static List<Vector3D> vectorsToBeDrawn;
+
+    @Override
+    public void settings()
+    {
+        noSmooth();
+        noLoop();
+
+        size(1000, 800);
+    }
+
+    @Override
+    public void setup()
+    {
+    }
+
+    @Override
+    public void draw()
+    {
+        background(220);
+        noFill();
+        drawEllipses();
+    }
+
+    public void drawEllipses()
+    {
+        System.out.println("Starting to draw.");
+
+        for(Vector3D vector : vectorsToBeDrawn)
+        {
+            int brightnessToUse = this.calculateEllipseBrightness(vector);
+
+            ellipse(vector.getX(), vector.getY(), 1,1);
+        }
+        System.out.println("Done drawing.");
+    }
+
+    private int calculateEllipseBrightness(Vector3D vector)
+    {
+        return 5 + ((int) vector.getZ() * 10);
+    }
+
+
     public static void main(String[] args)
     {
         try
@@ -32,19 +76,17 @@ public class Main
             GenericPair<Vector3D, Vector3D> smallestXYVectors = parsedFile.getVectorsWithSmallestXYs();
             GenericPair<Vector3D, Vector3D> largestXYVectors = parsedFile.getVectorsWithLargestXYs();
 
-            GenericPair<Integer, Integer> appletWidthHeight = new GenericPair<>(1000, 800);
+            GenericPair<Integer, Integer> appletWidthHeightMaximums = new GenericPair<>(1000, 800);
 
             ValueConverter converter = new ValueConverter(parsedFile.getSourceVectors(), smallestXYVectors,
-                                                          largestXYVectors, appletWidthHeight);
+                                                          largestXYVectors, appletWidthHeightMaximums);
 
-            BasicMapPlot basicPlot = new BasicMapPlot(converter.transformCoordinatesListToAppletPositions(),
-                                                      appletWidthHeight);
+            vectorsToBeDrawn = converter.transformCoordinatesListToAppletPositions();
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
-
+        PApplet.main(new String[]{Main.class.getName()});
     }
-        
 }
--- Opdracht3/src/main/src/main/java/Plotting/ValueConverter.java
+++ Opdracht3/src/main/src/main/java/Plotting/ValueConverter.java
@@ -3,7 +3,6 @@ package Plotting;
 import Datastructures.GenericPair;
 import Datastructures.Vector3D;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static processing.core.PApplet.map;
@@ -17,27 +16,29 @@ public class ValueConverter
     private List<Vector3D> valuesList;
     private GenericPair<Vector3D, Vector3D> smallestXYs;
     private GenericPair<Vector3D, Vector3D> largestXYs;
-    private GenericPair<Integer, Integer> appletWidthHeight;
+    private GenericPair<Integer, Integer> appletWidthHeightMaximums;
 
     public ValueConverter(List<Vector3D> valuesList,
                           GenericPair<Vector3D,Vector3D> smallestXYs,
                           GenericPair<Vector3D,Vector3D> largestXYs,
-                          GenericPair<Integer, Integer> appletWidthHeight)
+                          GenericPair<Integer, Integer> appletWidthHeightMaximums)
     {
         this.valuesList = valuesList;
         this.smallestXYs = smallestXYs;
         this.largestXYs = largestXYs;
-        this.appletWidthHeight = appletWidthHeight;
+        this.appletWidthHeightMaximums = appletWidthHeightMaximums;
     }
 
     public List<Vector3D> transformCoordinatesListToAppletPositions()
     {
+        System.out.println("Started transforming.");
         //I chose to transform an existing list instead of copying into a new one for performance reasons.
         //Not creating a new Vector here decreases readability, but increases performance.
         for(int i = 0; i < this.valuesList.size(); i++)
         {
             this.valuesList.set(i, convertCartesianVectorToAppletPosition(this.valuesList.get(i)));
         }
+        System.out.println("Done transforming.");
         return this.valuesList;
     }
 
@@ -50,12 +51,10 @@ public class ValueConverter
         float largestY = this.largestXYs.getRightValue().getY();
 
         float convertedX = map(unconvertedVector.getX(), smallestX, largestX,
-                               this.appletWidthHeight.getLeftValue(),
-                               this.appletWidthHeight.getRightValue());
+                0.0f, this.appletWidthHeightMaximums.getLeftValue());
 
         float convertedY = map(unconvertedVector.getY(), smallestY, largestY,
-                               this.appletWidthHeight.getLeftValue(),
-                               this.appletWidthHeight.getRightValue());
+                0.0f, this.appletWidthHeightMaximums.getRightValue());
 
         Vector3D returnVector = new Vector3D();
         returnVector.setxValue(convertedX);
