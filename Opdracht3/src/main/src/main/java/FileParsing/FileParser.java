463592be7de6a8dee4b475542b5bd1bac8721310
RobertKraaijeveld <0890289@hr.nl>
2016-06-02 19:28:20 +0200

    Found zadkine statue coordinate, found out values are RDH coordinates.

--- Opdracht3/src/main/src/main/java/FileParsing/FileParser.java
+++ Opdracht3/src/main/src/main/java/FileParsing/FileParser.java
@@ -37,29 +37,15 @@ public class FileParser
                    .map(e -> convertStringArrayToVector3D(e))
                    .collect(Collectors.toList());
 
-        if(this.searchForZadkineVector() == true)
-            System.out.println("Found X of zadkine");
-        else
-            System.out.println("not Found X of zadkine");
 
         System.out.println("Done parsing.");
         return new ParsedFile(Vector3DList);
     }
 
-    private boolean searchForZadkineVector() throws IOException
-    {
-        CharSequence cs1 = "int";
-        String test = "3930161";
-        boolean returnBoolean  = test.contains(cs1);
-                                 //Files
-                                 //   .lines(coordinateFilePath)
-                                 //   .anyMatch(line -> line.contains("3930161"));
-        return returnBoolean;
-    }
-
     private Vector3D convertStringArrayToVector3D(String input)
     {
         Vector3D returnVector = new Vector3D();
+
         returnVector.setxValue(Float.parseFloat(input.substring(0, input.indexOf(","))));
         returnVector.setyValue(Float.parseFloat(input.substring(input.indexOf(",") + 1, input.lastIndexOf(","))));
         returnVector.setzValue(Float.parseFloat(input.substring(input.lastIndexOf(",") + 1)));
